0376809fdffd23df28e97dc4e3b29abce6f5285b
Martin <martinbruun1995@gmail.com>
2023-09-23 13:42:13 +0200

    More styling for product list

--- src/scripts/makeProductElement.js
+++ src/scripts/makeProductElement.js
@@ -2,11 +2,25 @@ export function makeProductElement(image ,title, price, discountPrice) {
   const newDiv = document.createElement('div')
   const imgSrc = `../${image}`
   newDiv.classList.add('product')
-  newDiv.innerHTML = `
-    <Image src="${imgSrc}" alt="Product image"/>
+  if(discountPrice === 0) {
+    newDiv.innerHTML = `
+    <div class="img__con">
+      <Image src="${imgSrc}" alt="Product image"/>
+    </div>
     <h4>${title}</h4>
-    <p>${price}</p>
-    <p>${price}</p>
+    <p class="price__now">${price}</p>
   `
+  } else {
+    newDiv.innerHTML = `
+    <div class="img__con">
+      <Image src="${imgSrc}" alt="Product image"/>
+    </div>
+    <h4>${title}</h4>
+    <div class="product__price__con">
+      <p class="price__before">${price}</p>
+      <p class="price__now">${price - discountPrice}</p>
+    </div>
+  `
+  }
   return newDiv
 } 

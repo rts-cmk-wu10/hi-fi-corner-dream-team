ab4e217a0cea23e2fa8696408c1ee3a1f9376270
Martin <martinbruun1995@gmail.com>
2023-09-25 14:13:41 +0200

    Add: Woking filters and sorters

--- src/scripts/parameterMaker.js
+++ src/scripts/parameterMaker.js
@@ -2,33 +2,5 @@ export default function parameterMaker (method, param, value) {
   const url = new URL(window.location.href)
   const params = new URLSearchParams(url.search)
   params.set(param, value)
-
-  switch (method) {
-    case 'filter':
-
-      break
-
-    case 'search':
-
-      history.pushState(null, null, '?' + params.toString())
-      break
-
-    case 'paginate':
-
-      break
-
-    case 'sort':
-
-      break
-
-    case 'slice':
-
-      break
-
-    case 'relation':
-
-      break
-    default:
-      break
-  }
+  history.pushState(null, null, '?' + params.toString())
 }
